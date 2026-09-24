6dbb458cb78d192a3a2455bc4cdf2f6c3023ff7a
Telea <telea001@science-bs35.soliscom.uu.nl>
2026-09-25 00:27:37 +0200

    Small fix JS file

--- bibtexref/codemirror-bibtex.js
+++ bibtexref/codemirror-bibtex.js
@@ -1,16 +1,19 @@
 
-//Driver code to load the CodeMirror smart editor
-//customized for BibTeX
-
 import {EditorState} from "https://esm.sh/@codemirror/state";
 import {EditorView, basicSetup} from "https://esm.sh/codemirror";
 import {bibtex} from "https://esm.sh/codemirror-lang-bib";
+import {diagnosticCount, forEachDiagnostic} from "https://esm.sh/@codemirror/lint";
 
 
 window.CodeMirrorBib = {
     EditorState,
     EditorView,
     basicSetup,
-    bibtex
+    bibtex,
+    diagnosticCount,
+    forEachDiagnostic
 };
+
+
+
 
